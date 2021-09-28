505f0e116d8daff74631284be7fefd1737fd553d
Jason Lee <jason.lee@cern.ch>
2021-09-29 03:12:40 +0900

    corrected ge11 qc8 geo imp

---
+++ Geometry/python/GeometryQC8GE11_cff.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+from gemsw.Geometry.GeometryQC8GE11_cfi import *
+#from Geometry.CMSCommonData.cmsExtendedGeometry2026D86XML_cfi import *
+
+from Geometry.GEMGeometryBuilder.gemGeometry_cfi import *
+from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
+from Geometry.CommonTopologies.bareGlobalTrackingGeometry_cfi import *
+from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
