7b9afa8b1c54aacbb6a5ccb04ec8108f3878133c
Kyla Martinez <specialk0705@yahoo.com>
2023-11-20 02:47:53 +0100

    New Plots

--- DQM/plugins/QC8RecHitSource.cc
+++ DQM/plugins/QC8RecHitSource.cc
@@ -3,6 +3,7 @@
 using namespace std;
 using namespace edm;
 
+int nEvent = 0;
 QC8RecHitSource::QC8RecHitSource(const edm::ParameterSet &cfg)
   : hGEMGeom_(esConsumes()),
     hGEMGeomBeginRun_(esConsumes<edm::Transition::BeginRun>()) {
@@ -24,12 +25,15 @@ void QC8RecHitSource::bookHistograms(DQMStore::IBooker &booker, edm::Run const &
   for (auto chamber : GEMGeometry_->chambers()) {
     auto chamberId = chamber->id();
     auto ch = chamberId.chamber();
+    auto etaPart = GEMGeometry_->etaPartitions();
 
+    // Uploaded
     booker.setCurrentFolder("GE21QC8/Digi");
     mapDigiOcc_[ch] = booker.book2D(Form("digi_occ_ch%d", ch),
                                     Form("Digi Occupancy Chamber %d;strip;i#eta", ch),
                                     384, -0.5, 383.5,
                                     16, 0.5, 16.5);
+    //Uploaded 
     booker.setCurrentFolder("GE21QC8/RecHit");
     mapRecHitOcc_[ch] = booker.book2D(Form("rechit_occ_ch%d", ch),
                                       Form("Rechit Occupancy Chamber %d;x [cm]; y [cm]", ch),
@@ -39,10 +43,112 @@ void QC8RecHitSource::bookHistograms(DQMStore::IBooker &booker, edm::Run const &
                                           Form("Rechit Cluster Size Chamber %d;Cluster Size;i#eta", ch),
                                           maxClsSizeToShow_, 0.5, maxClsSizeToShow_ + 0.5,
                                           16, 0.5, 16.5);
+    //Not Uploaded 
+    // 1 hour
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc1hrEta1_4_[ch] = booker.book2D(Form("digiStrip_occ_1hr_ch%d_eta1_4", ch),
+                                    Form("DigiStrip Occupancy Chamber %d Eta 1-4;events;strip", ch),
+                                    6000, 0, 375000,
+                                    1536, 0, 1535);
+    // Not Uploaded
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc1hrEta5_8_[ch] = booker.book2D(Form("digiStrip_occ_1hr_ch%d_eta5_8", ch),
+                                    Form("DigiStrip Occupancy Chamber %d Eta 5-8;events;strip", ch),
+                                    6000, 0, 375000,
+                                    1536, 0, 1535);
+    //Not Uploaded
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc1hrEta9_12_[ch] = booker.book2D(Form("digiStrip_occ_1hr_ch%d_eta9_12", ch),
+                                     Form("DigiStrip Occupancy Chamber %d Eta 9-12;events;strip", ch),
+                                     6000, 0, 375000,
+                                     1536, 0, 1535);                                                         // Not Uploaded 
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc1hrEta13_16_[ch] = booker.book2D(Form("digiStrip_occ_1hr_ch%d_eta13_16", ch),
+                                      Form("DigiStrip Occupancy Chamber %d Eta 13-16;events;strip", ch),
+                                      6000, 0, 375000,
+                                      1536, 0, 1535);
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc5hrEta1_4_[ch] = booker.book2D(Form("digiStrip_occ_5hr_ch%d_eta1_4", ch),
+                                    Form("DigiStrip Occupancy Chamber %d Eta 1-4;events;strip", ch),
+                                    60000, 0, 2000000,
+                                    1536, 0, 1535);
+
+    // Not Uploaded
+    // 5 Hour
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc5hrEta5_8_[ch] = booker.book2D(Form("digiStrip_occ_5hr_ch%d_eta5_8", ch),
+                                    Form("DigiStrip Occupancy Chamber %d Eta 5-8;events;strip", ch),
+                                    60000, 0, 2000000,
+                                    1536, 0, 1535);
+    // Not Uploaded
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc5hrEta9_12_[ch] = booker.book2D(Form("digiStrip_occ_5hr_ch%d_eta9_12", ch),
+                                     Form("DigiStrip Occupancy Chamber %d Eta 9-12;events;strip", ch),
+                                     60000, 0, 2000000,
+                                     1536, 0, 1535);
+    // Not Uploaded 
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc5hrEta13_16_[ch] = booker.book2D(Form("digiStrip_occ_5hr_ch%d_eta13_16", ch),
+                                      Form("DigiStrip Occupancy Chamber %d Eta 13-16;events;strip", ch),
+                                      60000, 0, 2000000,
+                                      1536, 0, 1535);
+    // Not Uploaded 
+    // 10 hour 
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc10hrEta1_4_[ch] = booker.book2D(Form("digiStrip_occ_10hr_ch%d_eta1_4", ch),
+                                     Form("DigiStrip Occupancy Chamber %d Eta 1-4;events;strip", ch),
+                                     60000, 0, 3750000,
+                                     1536, 0, 1535);
+
+    // Not Uploaded
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc10hrEta5_8_[ch] = booker.book2D(Form("digiStrip_occ_10hr_ch%d_eta5_8", ch),
+                                     Form("DigiStrip Occupancy Chamber %d Eta 5-8;events;strip", ch),
+                                     60000, 0, 3750000,
+                                     1536, 0, 1535);
+    // Not Uploaded
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc10hrEta9_12_[ch] = booker.book2D(Form("digiStrip_occ_10hr_ch%d_eta9_12", ch),
+                                      Form("DigiStrip Occupancy Chamber %d Eta 9-12;events;strip", ch),
+                                      60000, 0, 3750000,
+                                      1536, 0, 1535);
+    // Not Uploaded 
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc10hrEta13_16_[ch] = booker.book2D(Form("digiStrip_occ_10hr_ch%d_eta13_16", ch),
+                                       Form("DigiStrip Occupancy Chamber %d Eta 13-16;events;strip", ch),
+                                       60000, 0, 3750000,
+                                       1536, 0, 1535);
+    // Not Uploaded 
+    // 24 hours 
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc24hrEta1_4_[ch] = booker.book2D(Form("digiStrip_occ_24hr_ch%d_eta1_4", ch),
+                                     Form("DigiStrip Occupancy Chamber %d Eta 1-4;events;strip", ch),
+                                     90000, 0, 8750000,
+                                     1536, 0, 1535);
+
+    // Not Uploaded
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc24hrEta5_8_[ch] = booker.book2D(Form("digiStrip_occ_24hr_ch%d_eta5_8", ch),
+                                     Form("DigiStrip Occupancy Chamber %d Eta 5-8;events;strip", ch),
+                                     90000, 0, 8750000,
+                                     1536, 0, 1535);
+    // Not Uploaded 
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc24hrEta9_12_[ch] = booker.book2D(Form("digiStrip_occ_24hr_ch%d_eta9_12", ch),
+                                      Form("DigiStrip Occupancy Chamber %d Eta 9-12;events;strip", ch),
+                                      90000, 0, 8750000,
+                                      1536, 0, 1535);
+    // Not Uploaded 
+    booker.setCurrentFolder("GE21QC8/DigiStrip");
+    mapDigiStripOcc24hrEta13_16_[ch] = booker.book2D(Form("digiStrip_occ_24hr_ch%d_eta13_16", ch),
+                                       Form("DigiStrip Occupancy Chamber %d Eta 13-16;events;strip", ch),
+                                       90000, 0, 8750000,
+                                       1536, 0, 1535);
   }
 }
 
 void QC8RecHitSource::analyze(edm::Event const &event, edm::EventSetup const &iSetup) {
+  nEvent++;
   /* GEM Geometry */
   edm::ESHandle<GEMGeometry> hGEMGeom = iSetup.getHandle(hGEMGeom_);
   const GEMGeometry* GEMGeometry_ = &*hGEMGeom;
@@ -67,8 +173,20 @@ void QC8RecHitSource::analyze(edm::Event const &event, edm::EventSetup const &iS
     for (auto digi = digiRange.first; digi != digiRange.second; ++digi) {
       auto strip = digi->strip();
       mapDigiOcc_[ch]->Fill(strip, ieta);
-    }
-    auto rechitRange = gemRecHit->get(gId);
+      if (ieta >= 1 && ieta <= 4) {
+        if (ieta == 2) {
+          strip = strip + 384;
+        }
+        if (ieta == 3) {
+          strip = strip + 768;
+        }
+        if (ieta == 4) {
+          strip = strip + 1152;
+        }
+        mapDigiStripOcc1hrEta1_4_[ch]->Fill(nEvent,strip);
+      }
+    } 
+   auto rechitRange = gemRecHit->get(gId);
     for (auto rechit = rechitRange.first; rechit != rechitRange.second; ++rechit) {
       auto cls_size = rechit->clusterSize();
       auto gPos = etaPart->surface().toGlobal(rechit->localPosition());
